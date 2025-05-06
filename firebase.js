b0cc3a8226300fd33ed737d4e467ba5b783e4cbb
Guilherme Reis <161058845+reis-g@users.noreply.github.com>
2025-05-06 13:11:48 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,16 @@
+// firebase.js
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDTQQSBYHF5Qdz-czJiHWoFvTH7TA66kCM",
+  authDomain: "hitorinhasapp.firebaseapp.com",
+  projectId: "hitorinhasapp",
+  storageBucket: "hitorinhasapp.firebasestorage.app",
+  messagingSenderId: "1012928568255",
+  appId: "1:1012928568255:android:d4de49f797f1ca9dcf9f0a"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
 
