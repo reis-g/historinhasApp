0cb5fb72957600f4dc41f8dd541571724c125248
Guilherme Reis <161058845+reis-g@users.noreply.github.com>
2025-05-06 13:12:38 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,1 +1,20 @@
+// App.js
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import CatalogoScreen from './screens/CatalogoScreen';
+import HistoriaScreen from './screens/HistoriaScreen';
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Catálogo">
+        <Stack.Screen name="Catálogo" component={CatalogoScreen} />
+        <Stack.Screen name="Historia" component={HistoriaScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
 
